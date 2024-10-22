fcba6f6ff331ad93fa9b669eb73fbe63b788d4a0
quhxuxm_quh <quhxuxm_quh@163.com>
2024-10-22 21:35:22 +0800

    init

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,8 +1,8 @@
 use bytes::Bytes;
 use rand::random;
-mod aes;
-mod error;
-mod rsa;
+pub mod aes;
+pub mod error;
+pub mod rsa;
 pub fn random_32_bytes() -> Bytes {
     let random_32_bytes = random::<[u8; 32]>();
     Bytes::from(random_32_bytes.to_vec())
