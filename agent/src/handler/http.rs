6eb5b693ae5f26a0a443e768e151fafa7c9b6c3a
quh <quhxuxm_quh@163.com>
2024-11-13 22:17:41 +0800

    init

--- agent/src/handler/http.rs
+++ agent/src/handler/http.rs
@@ -1,10 +1,22 @@
 use crate::bo::config::Config;
 use crate::error::AgentError;
 use crate::handler::HandlerRequest;
+use bytecodec::bytes::RemainingBytesDecoder;
+use httpcodec::{BodyDecoder, RequestDecoder};
 use std::sync::Arc;
 pub async fn handle_http_client_tcp_stream(
     config: Arc<Config>,
     request: HandlerRequest,
 ) -> Result<(), AgentError> {
+    let HandlerRequest {
+        client_tcp_stream,
+        session_token,
+        agent_encryption,
+        proxy_encryption,
+        http_client,
+        client_socket_addr
+    } = request;
+    let http_request_decoder = RequestDecoder::new(BodyDecoder::new(RemainingBytesDecoder::new()));
+    client_tcp_stream.peek()
     todo!()
 }
