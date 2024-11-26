20538c7165b8bf2d513dd8043714a59611aefad9
quhxuxm_quh <quhxuxm_quh@163.com>
2024-11-26 21:02:18 +0800

    refact - add connection pool heartbeat

--- agent/src/handler/http.rs
+++ agent/src/handler/http.rs
@@ -4,16 +4,15 @@ use crate::handler::{relay, tunnel_init, RelayRequest, TunnelInitHandlerResponse
 use bytecodec::bytes::{BytesEncoder, RemainingBytesDecoder};
 use bytecodec::io::IoDecodeExt;
 use bytecodec::{EncodeExt, ErrorKind};
-use bytes::{Buf, Bytes, BytesMut};
-use futures_util::StreamExt;
+use bytes::{Buf, BufMut, Bytes, BytesMut};
+use futures_util::{SinkExt, StreamExt};
 use httpcodec::{
     BodyDecoder, BodyEncoder, HttpVersion, ReasonPhrase, Request, RequestDecoder, RequestEncoder,
     Response, ResponseEncoder, StatusCode,
 };
 use ppaass_domain::address::UnifiedAddress;
-use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
-use tokio_util::codec::{Decoder, Framed};
+use tokio_util::codec::{Decoder, Encoder, Framed};
 use tracing::{debug, error};
 use url::Url;
 const CONNECT_METHOD: &str = "connect";
@@ -21,37 +20,57 @@ const OK_CODE: u16 = 200;
 const CONNECTION_ESTABLISHED: &str = "Connection Established";
 const HTTPS_PORT: u16 = 443;
 const HTTP_PORT: u16 = 80;
-struct HttpRequestDecoder {
-    http_request_decoder: RequestDecoder<BodyDecoder<RemainingBytesDecoder>>,
+
+#[derive(Debug, Default)]
+struct HttpCodec {
+    request_decoder: RequestDecoder<BodyDecoder<RemainingBytesDecoder>>,
+    response_encoder: ResponseEncoder<BodyEncoder<BytesEncoder>>,
 }
-impl HttpRequestDecoder {
+impl HttpCodec {
     pub fn new() -> Self {
-        Self {
-            http_request_decoder: RequestDecoder::default(),
-        }
+        Self::default()
     }
 }
-impl Decoder for HttpRequestDecoder {
+
+impl Decoder for HttpCodec {
     type Item = Request<Vec<u8>>;
     type Error = AgentError;
+
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        match self.http_request_decoder.decode_exact(src.chunk()) {
-            Ok(http_request) => Ok(Some(http_request)),
-            Err(e) => match e.kind() {
-                ErrorKind::IncompleteDecoding => Ok(None),
-                _ => {
-                    error!("Failed to decode http request: {}", e);
-                    Err(e.into())
+        let decode_result = match self.request_decoder.decode_exact(src.chunk()) {
+            Ok(decode_result) => decode_result,
+            Err(e) => {
+                return match e.kind() {
+                    ErrorKind::IncompleteDecoding => Ok(None),
+                    other_kind => {
+                        error!("Http agent fail to decode because of error: {other_kind:?}");
+                        Err(AgentError::ByteCodec(e))
+                    }
                 }
-            },
-        }
+            }
+        };
+        Ok(Some(decode_result))
     }
 }
+
+impl Encoder<Response<Vec<u8>>> for HttpCodec {
+    type Error = AgentError;
+
+    fn encode(&mut self, item: Response<Vec<u8>>, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        let encode_result = self
+            .response_encoder
+            .encode_into_bytes(item)
+            .map_err(|e| AgentError::ByteCodec(e))?;
+        dst.put_slice(encode_result.as_slice());
+        Ok(())
+    }
+}
+
 pub async fn handle_http_client_tcp_stream(
     mut client_tcp_stream: TcpStream,
     server_state: ServerState,
 ) -> Result<(), AgentError> {
-    let mut http_request_framed = Framed::new(&mut client_tcp_stream, HttpRequestDecoder::new());
+    let mut http_request_framed = Framed::new(&mut client_tcp_stream, HttpCodec::new());
     let http_request = http_request_framed
         .next()
         .await
@@ -110,11 +129,9 @@ pub async fn handle_http_client_tcp_stream(
             ReasonPhrase::new(CONNECTION_ESTABLISHED)?,
             vec![],
         );
-        let mut http_connect_success_response_encoder =
-            ResponseEncoder::<BodyEncoder<BytesEncoder>>::default();
-        let response_bytes = http_connect_success_response_encoder
-            .encode_into_bytes(http_connect_success_response)?;
-        client_tcp_stream.write_all(&response_bytes).await?;
+        http_request_framed
+            .send(http_connect_success_response)
+            .await?;
     }
     relay(
         RelayRequest {
