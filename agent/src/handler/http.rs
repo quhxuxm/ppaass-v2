c5696137eec728ed49c648dd8f4b029c95f5875a
quh <quhxuxm_quh@163.com>
2024-11-27 09:22:58 +0800

    Re-do because of HTTP is blocked by company

--- agent/src/handler/http.rs
+++ agent/src/handler/http.rs
@@ -3,78 +3,54 @@ use crate::error::AgentError;
 use crate::handler::{relay, tunnel_init, RelayRequest, TunnelInitHandlerResponse};
 use bytecodec::bytes::{BytesEncoder, RemainingBytesDecoder};
 use bytecodec::io::IoDecodeExt;
-use bytecodec::{EncodeExt, ErrorKind};
-use bytes::{Buf, BufMut, Bytes, BytesMut};
-use futures_util::{SinkExt, StreamExt};
+use bytecodec::{Decode, DecodeExt, EncodeExt, Eos, ErrorKind};
+use bytes::{Buf, Bytes};
 use httpcodec::{
-    BodyDecoder, BodyEncoder, HttpVersion, ReasonPhrase, Request, RequestDecoder, RequestEncoder,
+    BodyDecoder, BodyEncoder, HttpVersion, ReasonPhrase, RequestDecoder, RequestEncoder,
     Response, ResponseEncoder, StatusCode,
 };
 use ppaass_domain::address::UnifiedAddress;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
-use tokio_util::codec::{Decoder, Encoder, Framed};
-use tracing::{debug, error};
+use tracing::debug;
 use url::Url;
 const CONNECT_METHOD: &str = "connect";
 const OK_CODE: u16 = 200;
 const CONNECTION_ESTABLISHED: &str = "Connection Established";
 const HTTPS_PORT: u16 = 443;
 const HTTP_PORT: u16 = 80;
-
-#[derive(Debug, Default)]
-struct HttpCodec {
-    request_decoder: RequestDecoder<BodyDecoder<RemainingBytesDecoder>>,
-    response_encoder: ResponseEncoder<BodyEncoder<BytesEncoder>>,
-}
-impl HttpCodec {
-    pub fn new() -> Self {
-        Self::default()
-    }
-}
-
-impl Decoder for HttpCodec {
-    type Item = Request<Vec<u8>>;
-    type Error = AgentError;
-
-    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        let decode_result = match self.request_decoder.decode_exact(src.chunk()) {
-            Ok(decode_result) => decode_result,
-            Err(e) => {
-                return match e.kind() {
-                    ErrorKind::IncompleteDecoding => Ok(None),
-                    other_kind => {
-                        error!("Http agent fail to decode because of error: {other_kind:?}");
-                        Err(AgentError::ByteCodec(e))
-                    }
-                }
-            }
-        };
-        Ok(Some(decode_result))
-    }
-}
-
-impl Encoder<Response<Vec<u8>>> for HttpCodec {
-    type Error = AgentError;
-
-    fn encode(&mut self, item: Response<Vec<u8>>, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        let encode_result = self
-            .response_encoder
-            .encode_into_bytes(item)
-            .map_err(|e| AgentError::ByteCodec(e))?;
-        dst.put_slice(encode_result.as_slice());
-        Ok(())
-    }
-}
-
+const HTTP_REQUEST_BUF_LEN: usize = 65536;
 pub async fn handle_http_client_tcp_stream(
     mut client_tcp_stream: TcpStream,
     server_state: ServerState,
 ) -> Result<(), AgentError> {
-    let mut http_request_framed = Framed::new(&mut client_tcp_stream, HttpCodec::new());
-    let http_request = http_request_framed
-        .next()
-        .await
-        .ok_or(AgentError::ClientTcpConnectionExhausted)??;
+    let mut request_decoder: RequestDecoder<BodyDecoder<RemainingBytesDecoder>> = Default::default();
+    let mut request_decode_buf = Vec::new();
+    let http_request = loop {
+        let mut request_stream_read_buf = [0u8; HTTP_REQUEST_BUF_LEN];
+        let size = client_tcp_stream.read(&mut request_stream_read_buf).await?;
+        if size >= HTTP_REQUEST_BUF_LEN {
+            request_decode_buf.extend(request_stream_read_buf);
+            continue;
+        } else {
+            request_decode_buf.extend(request_stream_read_buf);
+            let request_decode_buf_read = request_decode_buf.reader();
+            let request = match request_decoder.decode_exact(request_decode_buf_read) {
+                Ok(request) => request,
+                Err(e) => {
+                    match e.kind() {
+                        ErrorKind::IncompleteDecoding => {
+                            continue;
+                        }
+                        _ => {
+                            return Err(e.into());
+                        }
+                    }
+                }
+            };
+            break request;
+        }
+    };
     let request_method = http_request.method().to_string();
     let (destination_address, initial_http_request_bytes) =
         if request_method.to_lowercase() == CONNECT_METHOD {
@@ -129,9 +105,11 @@ pub async fn handle_http_client_tcp_stream(
             ReasonPhrase::new(CONNECTION_ESTABLISHED)?,
             vec![],
         );
-        http_request_framed
-            .send(http_connect_success_response)
-            .await?;
+        let mut http_connect_success_response_encoder =
+            ResponseEncoder::<BodyEncoder<BytesEncoder>>::default();
+        let response_bytes = http_connect_success_response_encoder
+            .encode_into_bytes(http_connect_success_response)?;
+        client_tcp_stream.write_all(&response_bytes).await?;
     }
     relay(
         RelayRequest {
@@ -144,6 +122,6 @@ pub async fn handle_http_client_tcp_stream(
         },
         server_state,
     )
-    .await?;
+        .await?;
     Ok(())
 }
