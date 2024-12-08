ca9f6732de1237e9eadd75afdc8857b4e920b875
quhxuxm_quh <quhxuxm_quh@163.com>
2024-12-08 08:43:00 +0800

    REFACT - optimize pool

--- script/start-proxy.sh
+++ script/start-proxy.sh
@@ -1,3 +1,17 @@
+#!/bin/sh
+a=0
+while [ $a -lt 5 ];
+do
+    process_id=$(ps -ef | grep "ppaass-v2-proxy" | grep -v grep | awk '{print $2}')
+    if [ -z "$process_id"]; then
+        echo "No ppaass-v2-proxy process"
+    else
+        echo "Found ppaass-v2-proxy process: $process_id"
+        kill -9 $process_id
+    fi
+    a=`expr $a + 1`
+    sleep 2
+done
+
 ulimit -n 409600
-sudo ps -ef | grep ppaass-v2-proxy | grep -v grep | awk '{print $2}' | xargs sudo kill -9
 RUST_BACKTRACE=1 ./ppaass-v2-proxy -c resources/config.toml
