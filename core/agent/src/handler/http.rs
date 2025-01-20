9219c9d454bdf2b29a4f4c3f1b3074cf6241f988
quhxuxm_quh <quhxuxm_quh@163.com>
2025-01-20 20:58:04 +0800

    refact

--- core/agent/src/handler/http.rs
+++ core/agent/src/handler/http.rs
@@ -3,13 +3,14 @@ use crate::error::AgentError;
 use crate::handler::{relay, tunnel_init, RelayRequest, TunnelInitHandlerResponse};
 use bytecodec::bytes::{BytesEncoder, RemainingBytesDecoder};
 use bytecodec::io::IoDecodeExt;
-use bytecodec::{EncodeExt, ErrorKind};
+use bytecodec::EncodeExt;
 use bytes::{Buf, Bytes};
 use httpcodec::{
-    BodyDecoder, BodyEncoder, HttpVersion, ReasonPhrase, RequestDecoder, RequestEncoder, Response,
-    ResponseEncoder, StatusCode,
+    BodyDecoder, BodyEncoder, HeaderField, HttpVersion, ReasonPhrase, Request, RequestDecoder,
+    RequestEncoder, Response, ResponseEncoder, StatusCode,
 };
 use ppaass_domain::address::UnifiedAddress;
+use pretty_hex::pretty_hex;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tracing::{debug, error};
@@ -19,37 +20,60 @@ const OK_CODE: u16 = 200;
 const CONNECTION_ESTABLISHED: &str = "Connection Established";
 const HTTPS_PORT: u16 = 443;
 const HTTP_PORT: u16 = 80;
-const HTTP_REQUEST_BUF_LEN: usize = 65536;
+const HTTP_REQUEST_BUF_LEN: usize = 512;
 pub async fn handle_http_client_tcp_stream(
     mut client_tcp_stream: TcpStream,
     server_state: ServerState,
 ) -> Result<(), AgentError> {
-    let mut request_decoder: RequestDecoder<BodyDecoder<RemainingBytesDecoder>> =
-        Default::default();
     let mut request_decode_buf = Vec::new();
-    let http_request = loop {
+    loop {
         let mut request_stream_read_buf = [0u8; HTTP_REQUEST_BUF_LEN];
         let read_amount = client_tcp_stream.read(&mut request_stream_read_buf).await?;
+        if 0 == read_amount {
+            break;
+        }
         request_decode_buf.extend(&request_stream_read_buf[..read_amount]);
-        let request_decode_buf_read = request_decode_buf.reader();
-        let request = match request_decoder.decode_exact(request_decode_buf_read) {
-            Ok(request) => request,
-            Err(e) => match e.kind() {
-                ErrorKind::IncompleteDecoding => {
-                    continue;
+        if read_amount < HTTP_REQUEST_BUF_LEN {
+            break;
+        }
+    }
+    let mut request_decoder: RequestDecoder<BodyDecoder<RemainingBytesDecoder>> =
+        Default::default();
+    let request_decode_buf_read = request_decode_buf.reader();
+    let client_request = request_decoder.decode_exact(request_decode_buf_read)?;
+    let (client_request, client_body) = client_request.take_body();
+    let client_request_method = client_request.method();
+    let client_request_target = client_request.request_target();
+    let client_http_version = client_request.http_version();
+    let client_request_header = client_request.header();
+    let client_request_header_fields = client_request_header.fields();
+
+    let mut proxy_request = Request::new(
+        client_request_method,
+        client_request_target,
+        client_http_version,
+        client_body,
+    );
+    client_request_header_fields.for_each(|header_field| {
+        if header_field.name().eq_ignore_ascii_case("Proxy-Connection") {
+            let connection_field = match HeaderField::new("Connection", header_field.value()) {
+                Ok(connection_field) => connection_field,
+                Err(e) => {
+                    error!("Fail to add Connection field: {}", e);
+                    return;
                 }
-                _ => {
-                    error!("Fail to decode http request: {}", e);
-                    return Err(e.into());
-                }
-            },
-        };
-        break request;
-    };
-    let request_method = http_request.method().to_string();
+            };
+            proxy_request.header_mut().add_field(connection_field);
+            return;
+        }
+        proxy_request.header_mut().add_field(header_field);
+    });
+
+    println!("Receive http request:\n{}", pretty_hex(&request_decode_buf));
+    let request_method = proxy_request.method().to_string();
     let (destination_address, initial_http_request_bytes) =
         if request_method.to_lowercase() == CONNECT_METHOD {
-            let request_target = http_request.request_target();
+            let request_target = proxy_request.request_target();
             let request_url = Url::parse(format!("https://{}", request_target.as_str()).as_str())?;
             debug!("Receive https request: {}", request_url);
             //HTTPS request with proxy
@@ -67,12 +91,11 @@ pub async fn handle_http_client_tcp_stream(
             )
         } else {
             //HTTP request with proxy
-            let request_target = http_request.request_target();
+            let request_target = proxy_request.request_target();
             let request_url = Url::parse(request_target.as_str())?;
-            debug!("Receive http request: {}", request_url);
             let mut http_data_encoder = RequestEncoder::<BodyEncoder<BytesEncoder>>::default();
             let initial_http_request_bytes: Bytes =
-                http_data_encoder.encode_into_bytes(http_request)?.into();
+                http_data_encoder.encode_into_bytes(proxy_request)?.into();
             (
                 UnifiedAddress::Domain {
                     host: request_url
